20c88c1ac773c5e1cdf8bed7860c3d6f34a07dbd
robert masen <r@robertmasen.pizza>
2018-04-26 13:10:13 -0500

    Working example

---
+++ index.js
@@ -0,0 +1,29 @@
+const mod = import('./hello_world');
+let hw;
+mod.then(wasm => {
+    hw = wasm;
+    let btn = document.getElementById('say-hello');
+    if (!btn) return console.error('No button found');
+    btn.addEventListener('click', sayHello);
+});
+
+function sayHello() {
+    console.log('saying hello');
+    let input = document.getElementById('your-name');
+    if (!input || input.value == '') {
+        updateMessage(hw.generate_greeting());
+    } else {
+        updateMessage(hw.generate_custom_greeting(input.value));
+    }
+}
+
+function updateMessage(msg) {
+    console.log('updating message')
+    let div = document.getElementById('message-container');
+    while (div.hasChildNodes()) {
+        div.removeChild(div.lastChild);
+    }
+    let h1 = document.createElement('h1');
+    h1.appendChild(document.createTextNode(msg))
+    div.appendChild(h1);
+}
